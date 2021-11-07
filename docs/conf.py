f62400daee4251428579c75ee04133d833e1d8c9
Ikram Ali <mrikram1989@gmail.com>
2021-11-07 11:56:31 +0500

    [config] bug fixed.

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ html_static_path = ['_static']
 html_theme_options = {
     'logo_only': True,
 }
-html_logo = '_static/statistics.png'
+html_logo = '_static/statistics.jpg'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
