5a54fb9fc39cab7b5c8765041a51fbe63c546c13
Ikram Ali <mrikram1989@gmail.com>
2021-11-07 11:33:59 +0500

    [config] new file configs added.

--- docs/conf.py
+++ docs/conf.py
@@ -62,5 +62,24 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    'logo_only': True,
+}
+html_logo = '_static/statistics.png'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+
+html_context = {
+    'css_files': [
+        '_static/statistics.css'
+    ]
+}
+
 # The master toctree document.
 master_doc = 'index'
