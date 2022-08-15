033c962175284efa684e8165f70d5ca82455c435
ikram ali <mrikram1989@gmail.com>
2022-08-16 00:27:33 +0500

    New changes.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx_inline_tabs',
+    'myst_nb',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,12 +46,13 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+# source_suffix = ['.rst', '.md']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+nitpicky = True
 
 # -- Options for HTML output -------------------------------------------------
 
